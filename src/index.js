b8e5a5fd0402caa901bb46271f7a90d8f62325f8
lucas333457 <patomaloquero235@gmail.com>
2025-07-14 11:10:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TreinoTracker from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TreinoTracker />
+    <App />
   </React.StrictMode>
 );
